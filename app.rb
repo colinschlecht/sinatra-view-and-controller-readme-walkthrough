8bc817ce4301698679d73310268eeed91140f6c1
Colin Schlecht <schlechtcolin@yahoo.com>
2021-02-01 16:11:42 -0800

    commit tree 309d291add196c3f4a2d8f66b3ec6ca2d6e4dbc1

--- app.rb
+++ app.rb
@@ -1,16 +1,28 @@
 require_relative 'config/environment'
 
+
 class App < Sinatra::Base
+
+  get '/' do 
+    "hello"
+  end
+
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
+    originalstring = params["string"]
+    @reversedstring = originalstring.reverse
     erb :reversed
   end
+#Note: Instance variables are ONLY passed from the controller method where 
+    #they are created to the view that is rendered, not between controller methods.
+
+
 
   get '/friends' do
-    # Write your code here!
-
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :friends
   end
 end
